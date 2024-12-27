a0084a8c4724f5a4e6de0234ed11351bbf42a364
Evan-OR <tehEvster@gmail.com>
2024-12-27 11:58:35 +0000

    fixed auto correct stuff

--- backend/app/controllers/images_controller.rb
+++ backend/app/controllers/images_controller.rb
@@ -1,25 +1,25 @@
 class ImagesController < ApplicationController
-  require 'mini_magick'
+  require "mini_magick"
 
   def create
     # Validate file presence
-    return render json: { error: 'No file provided' }, status: :unprocessable_entity unless params[:image]
+    return render json: { error: "No file provided" }, status: :unprocessable_entity unless params[:image]
 
     # Process the uploaded file
     uploaded_file = params[:image]
     custom_file_name = params[:customFileName]+".jpg"
     original_filename = uploaded_file.original_filename
-    file_path = Rails.root.join('public', 'img', original_filename)
+    file_path = Rails.root.join("public", "img", original_filename)
 
     # Save the original image locally
-    File.open(file_path, 'wb') do |file|
+    File.open(file_path, "wb") do |file|
       file.write(uploaded_file.read)
     end
 
     # Resize the image
-    resized_path = Rails.root.join('public', 'img', "#{custom_file_name}")
+    resized_path = Rails.root.join("public", "img", "#{custom_file_name}")
     image = MiniMagick::Image.open(file_path)
-    image.resize '512x512'
+    image.resize "512x512"
     image.write(resized_path)
 
     # Save metadata to the database
@@ -31,11 +31,11 @@ class ImagesController < ApplicationController
 
   def show
     image = Image.find_by(file_path: params[:file_path])
-    
+
     if image
-      send_file Rails.root.join('public', 'img', image.file_path), type: 'image/jpeg', disposition: 'inline'
+      send_file Rails.root.join("public", "img", image.file_path), type: "image/jpeg", disposition: "inline"
     else
-      render json: { error: 'Image not found' }, status: :not_found
+      render json: { error: "Image not found" }, status: :not_found
     end
   end
 end
