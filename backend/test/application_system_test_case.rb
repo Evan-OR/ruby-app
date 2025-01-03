c34d5ca399b92056951753c6b1a3f647e0f202c4
Evan-OR <tehEvster@gmail.com>
2025-01-03 12:01:23 +0000

    system testse

--- backend/test/application_system_test_case.rb
+++ backend/test/application_system_test_case.rb
@@ -2,5 +2,5 @@ require "test_helper"
 require "capybara/rails"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [ 1000, 1000 ]
+  driven_by :selenium, using: :headless_chrome, screen_size: [ 1000, 1000 ]
 end
